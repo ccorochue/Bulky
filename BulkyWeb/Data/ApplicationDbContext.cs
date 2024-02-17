519fa363fc12dc231d758396119179c869b2b748
Daniel Soto Corochué <ccorochue@gmail.com>
2024-02-16 20:42:09 -0500

    Category elements displayed on view

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -11,5 +11,14 @@ namespace BulkyWeb.Data
         }
 
         public DbSet<Category> categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { id = 1, name = "Action", display_order = 1 },
+                new Category { id = 2, name = "SciFi", display_order = 2 },
+                new Category { id = 3, name = "History", display_order = 3 }
+                );
+        }
     }
 }
