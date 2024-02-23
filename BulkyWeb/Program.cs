715d061826f80039906695aeff7f02b4e4a94281
Daniel Soto Corochué <ccorochue@gmail.com>
2024-02-22 22:21:02 -0500

    Adding aspnet identity

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -2,6 +2,7 @@ using Bulky.DataAcess.Data;
 using Bulky.DataAcess.Repository;
 using Bulky.DataAcess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,10 @@ builder.Services.AddControllersWithViews();
 
 // To use Entity Framework and the differents connectionStrings
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddRazorPages();
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+
 
 // Implemented services
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
@@ -28,11 +33,13 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=customer}/{controller=home}/{action=index}/{id?}");
+
 
 app.Run();
