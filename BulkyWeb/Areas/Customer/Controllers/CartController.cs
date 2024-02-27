27f1a49249bace7cbebe2c9139c3ffeaa7419f75
Daniel Soto Corochué <ccorochue@gmail.com>
2024-02-26 20:10:53 -0500

    Add or remove quantity of products on cart

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -13,7 +13,6 @@ namespace BulkyWeb.Areas.Customer.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
 
-
         public CartController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
@@ -31,6 +30,76 @@ namespace BulkyWeb.Areas.Customer.Controllers
             shoppingCartVM.OrderTotal = shoppingCartVM.ShoppingCartList.Sum(i => i.Price * i.Count);
 
             return View(shoppingCartVM);
+        }
+
+        public IActionResult Plus(int id)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCart shoppingCart = _unitOfWork.ShoppingCart.Get(i => i.ProductId == id && i.ApplicationUserId == userId);
+            shoppingCart.Count++;
+            shoppingCart.Price = ProductPrice(id, shoppingCart.Count);
+
+            _unitOfWork.ShoppingCart.Update(shoppingCart);
+            _unitOfWork.Save();
+
+            return RedirectToAction("index");
+        }
+
+        public IActionResult Minus(int id)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCart shoppingCart = _unitOfWork.ShoppingCart.Get(i => i.ProductId == id && i.ApplicationUserId == userId);
+
+            if (shoppingCart.Count <= 1)
+            {
+                _unitOfWork.ShoppingCart.Delete(shoppingCart);
+            }
+            else
+            {
+                shoppingCart.Count--;
+                shoppingCart.Price = ProductPrice(id, shoppingCart.Count);
+
+                _unitOfWork.ShoppingCart.Update(shoppingCart);
+            }
+
+            _unitOfWork.Save();
+
+            return RedirectToAction("index");
+        }
+
+        public IActionResult Remove(int id)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCart shoppingCart = _unitOfWork.ShoppingCart.Get(i => i.ProductId == id && i.ApplicationUserId == userId);
+
+            _unitOfWork.ShoppingCart.Delete(shoppingCart);
+            _unitOfWork.Save();
+
+            return RedirectToAction("index");
+        }
+
+        private double? ProductPrice(int productId, int quantity)
+        {
+            Product product = _unitOfWork.Product.Get(i => i.Id == productId);
+
+            if (quantity <= 50)
+            {
+                return product.Price;
+            }
+            else if (quantity <= 100)
+            {
+                return product.Price50;
+            }
+            else
+            {
+                return product.Price100;
+            }
         }
     }
 }
